054f0e792b3e787707ce946d835fb87d93b43832
Georgi-Penev <george.p.penev@gmail.com>
2025-01-12 07:40:41 +0200

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
+import Navbar from './components/Navbar'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const rubik = Rubik({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={rubik.className}>
+        <Navbar/> 
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-      <main>
-        <h2>Dashboard</h2>
-      </main>
+    <main>
+      <h2>Dashboard</h2>
+      <p>
+        Deserunt occaecat culpa ullamco adipisicing deserunt nisi. Amet nostrud
+        consectetur magna sint. Magna cupidatat consectetur dolore duis
+        excepteur ad ut cupidatat anim.
+      </p>
+
+      <div className="flex justify-center my-8">
+        <Link href="/tickets">
+          <button className="btn-primary">View Tickets</button>
+        </Link>
+      </div>
+
+      <h2>Company Updates</h2>
+
+      <div className="card">
+        <h3>New member of the web dev team...</h3>
+        <p>Commodo occaecat et sunt veniam amet ea qui deserunt.</p>
+      </div>
+      <div className="card">
+        <h3>New website live!</h3>
+        <p>Laboris non in do sunt ad ipsum nisi.</p>
+      </div>
+    </main>
   );
 }
